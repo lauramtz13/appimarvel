408a44a61ae3d8380793be1e1ef177cabe939683
Laura Martínez Reyes <57735597+lauramtz13@users.noreply.github.com>
2023-06-07 16:53:48 -0600

    Add files via upload

---
+++ src/Components/Main.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { Card } from "./Card"
+import axios from "axios"
+import { useState } from "react"
+import { useEffect } from "react"
+export const Main = () => {
+  const [url,setUrl]=useState("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=19463d5b9c33c9e793a7fd3dc13b128b&hash=fd54cc533afc8eac0779b4c6eddfadf6")
+  const [item,setItem]=useState();
+  const [search,setSearch]=useState("");
+  useEffect(()=>{
+    const fetch=async()=>{
+      const res=await axios.get(url)
+      setItem(res.data.data.results);
+    }
+    fetch();
+  },[url])
+  
+  const searchMarvel=()=>{
+    setUrl(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&ts=1&apikey=2e1cdeec426ae323484f29024084c206&hash=d516513ba95b9407c7aca0f73b241f8a`)
+  }
+
+  return (
+    <>
+        <div className="header">
+            <div className="bg">
+                <img src="./Images/heroes.jpg" alt="" />
+            </div>
+            <div className="search-bar">
+              
+                <input type="search" placeholder='Buscar'
+                 className='search'
+                 onChange={e=>setSearch(e.target.value)}
+                 onKeyPress={searchMarvel}/>
+            </div>
+        </div>
+       <div className="content">
+         
+        {
+          (!item)?<p>Not Found</p>:<Card data={item}/>
+        }
+       </div>
+    </>
+  )
+}
